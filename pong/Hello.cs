9cefe443e2f2ccb2ce655af8e7c66d18495280c1
ReinisTeBija <rdzintars4@edu.riga.lv>
2026-09-14 08:50:54 +0300

    Hello skripts

--- pong/Hello.cs
+++ pong/Hello.cs
@@ -3,13 +3,18 @@ using System;
 
 public partial class Hello : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	private int counter = 0;
+	private string playerName = "Pong Player";
+	private Vector2 spawnPosition = new Vector2(640, 360);
 	public override void _Ready()
 	{
 		GD.Print("Sveiks no C#!");
+		GD.Print(counter);
+		GD.Print(playerName);
+		GD.Print(spawnPosition);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
 	public override void _Process(double delta)
 	{
 	}
--- pong/README.md
+++ pong/README.md
@@ -8,3 +8,8 @@ Inspector - labajā
 "Koka" sakārotšana atvieglo dažādus spēles elementus un atļauj tos vieglāk atrast pēc tam
 
 Dotnet versija: 10.0.401
+
+## noņemot ; no rindas 'GD.print...' parādās kļūda Compiler Error CS1002
+
+## Python - python nosak vērtību automātiski, piemēram counter = 0, bet C# vajag ievadīt int counter = 0;
+## C# - Katras līnijas beigās jābūt ';', C# obligāti jāveic ir 'build', lai notiktu izmaiņas projektā 
